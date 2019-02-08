eb2a6a686c1045915e253f878f1d58db51f5d66a
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-02-08 10:52:15 +0100

    start defining components

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
-import promiseMiddleware from 'redux-promise';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
@@ -23,15 +23,28 @@ const reducers = combineReducers({
 });
 
 const initialState = {
-  messages: [],
+  messages: [
+    {
+      author: "anonymous92",
+      content: "Hello world!",
+      created_at: "2017-09-26T23: 03: 16.365Z"
+    },
+    {
+      author: "anonymous77",
+      content: "My name is anonymous77",
+      created_at: "2017-09-26T23: 03: 21.194Z"
+    }
+  ],
   channels: ['general', 'react', 'paris'],
   selectedChannel: 'general',
   currentUser: prompt('What is your username?')
 };
 
+const middleWares = applyMiddleware(logger, reduxPromise);
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, applyMiddleware(logger, promiseMiddleware))}>
+  <Provider store={createStore(reducers, initialState, middleWares)}>
     <App />
   </Provider>,
   document.getElementById('root')
